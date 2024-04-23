6b53487f55009919a5767fa7fe64f4d7bee8b9b2
Pedro <pedrophnc@gmail.com>
2024-04-23 19:32:40 -0400

    Primeiro commit

---
+++ src/main/java/br/unigran/git_aula/Git_aula.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.unigran.git_aula;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Git_aula {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
